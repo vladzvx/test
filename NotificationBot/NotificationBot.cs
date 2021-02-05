61c46d7d75d7d02deadfd471353d4581ffd365ad
vladislav <wladzwer@mail.ru>
2021-02-05 18:16:49 +0300

    sequrity fix

--- NotificationBot/NotificationBot.cs
+++ NotificationBot/NotificationBot.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
 
 namespace NotificationBot
 {
@@ -10,13 +12,26 @@ namespace NotificationBot
             
         }
 
+        public override void PrivateChatProcessing(Message message, ref bool continuation)
+        {
+            base.PrivateChatProcessing(message, ref continuation);
+            if (!dBWorker.check_chat_activation(message.Chat.Id, token))
+            {
+                sender_to_tg.Put(factory.CreateMessage(message.Chat.Id,
+                    "Активируйте бота отправив команду вида \n /activate 111:AAA \n где 111:AAA - токен этого бота."));
+            }
+        }
+
         public void SendMessages(string message)
         {
             List<long> chats = dBWorker.get_active_private_chats(token);
             foreach (long g in chats)
             {
-                if (!dBWorker.check_user_ban((int)g, g, token))
+                if (dBWorker.check_chat_activation(g, token))
+                {
                     sender_to_tg.Put(factory.CreateMessage(g, message));
+                }
+                
             }
         }
     }
