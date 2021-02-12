48c963f20373df36e78fe9c14125035256ca486e
vladislav <wladzwer@mail.ru>
2021-02-12 18:41:00 +0300

    deferred bot fix

--- DataBase/DB_migration_script.sql
+++ DataBase/DB_migration_script.sql
@@ -1,3 +1,39 @@
+alter table tasks drop constraint tasks_source_message_id_fkey;
+
+CREATE OR REPLACE FUNCTION add_message_to_task() RETURNS trigger as
+$$
+    declare
+        bot_uid bigint;
+    begin
+        bot_uid = (select bots.bot_user_id from bots where bots.bot_id=new.bot_id);
+        if not bot_uid=new.user_id  then
+            update public.tasks SET source_message_id=new.message_id
+                where bot_id=new.bot_id and source_chat = new.chat_id and task_status=1 and source_message_id is null;
+        end if;
+        return null;
+    end;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION get_active_tasks(bot_token text ) RETURNS TABLE (_source_chat bigint,
+                                                                            _message_id bigint,
+                                                                            _task_type text,
+                                                                            _target_chat bigint,
+                                                                            text varchar(4000),
+                                                                            caption varchar(1000),
+                                                                            media_id text,
+                                                                            media_group_id text,
+                                                                            task_id int,
+                                                                            _action_time timestamp,
+                                                                            chat_name text) as
+$$
+    begin
+        RETURN QUERY
+            select distinct source_chat,message_id,task_type,target_chat,messages.text,messages.caption,messages.media_id,messages.media_group_id,tasks.task_id, tasks.action_time,c.name from tasks inner join messages on tasks.source_message_id=messages.message_id inner join chats c on tasks.source_chat = c.chat_id
+            where tasks.bot_id=get_bot_id(bot_token) and action_time<CURRENT_TIMESTAMP and task_status<3;
+    end;
+$$ LANGUAGE plpgsql;
+
+
 drop function  task_complited(source_chat_id bigint, target_chat_id bigint, bot_token text);
 drop function  task_not_complited(source_chat_id bigint, target_chat_id bigint, bot_token text);
 
